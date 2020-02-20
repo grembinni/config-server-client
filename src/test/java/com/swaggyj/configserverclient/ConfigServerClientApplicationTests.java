40677e78d8ba80ca13f0e4a73898cef687b373ca
Jeremy Hurdle <jeremy.hurdle@gmail.com>
2020-02-20 14:44:39 -0600

    initial commit

---
+++ src/test/java/com/swaggyj/configserverclient/ConfigServerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaggyj.configserverclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
